f2b1779712e04b5ca05badabbd850998c74ddfd8
N.Sementsov <sementsovnikita2@gmail.com>
2026-03-06 23:35:48 +0100

    fixdc

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   base: '/your-energy/',
   build: {
-    outDir: 'dist',
+    outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
