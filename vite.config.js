23f4e8521156d94d518c4da88b19e6d362f24f1f
N.Sementsov <sementsovnikita2@gmail.com>
2026-03-06 23:44:19 +0100

    fxbp

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/your-energy/',
+  base: '/hw_js_ns/',
   build: {
     outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
